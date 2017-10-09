597532bc3716c5aea425e06339ff636e1cb10fad
Luke Rielley <luke.rielley@gmail.com>
2017-10-09 13:17:24 +1100

    Updates

--- nodeexamples/fromcallback/index.js
+++ nodeexamples/fromcallback/index.js
@@ -1,19 +1,10 @@
 const fs = require('fs');
 const Rx = require('rxjs/Rx');
 
-console.log(`Welcome to the example`);
-
-let simple = Rx.Observable.from([1,2,3]);
-
-simple.subscribe((x) => {
-    console.log(`Simple output: ${x}`);
-});
-
 // Natural Callback Example
 fs.exists("./samplefile.txt", function (exists) {
     console.log(`Does 'samplefile.txt' exist? = ${exists}`);
 });
-
 
 // RxJS Wrapper of a Callback
 var exists = Rx.Observable.bindCallback(fs.exists);
